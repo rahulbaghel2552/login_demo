a77b4ca9453a1f48933af8540023279c84e902cd
rahul baghel <rahulbaghel6513@gmail.com>
2021-02-01 14:45:43 +0530

    file updated

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-
+mongodb+srv://rahul_baghel:<password>@cluster0.mrbxl.mongodb.net/<dbname>?retryWrites=true&w=majority
 mongoose
-  .connect("mongodb://localhost:27017/logindemo", {
+  .connect("mongodb+srv://rahul_baghel:Rahul$125@cluster0.mrbxl.mongodb.net/logindemo?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,

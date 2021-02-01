4fd74087283f5256b9c823f72563012bd5a23ab1
rahulbaghel2552 <77883642+rahulbaghel2552@users.noreply.github.com>
2021-02-01 14:56:51 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-mongodb+srv://rahul_baghel:<password>@cluster0.mrbxl.mongodb.net/<dbname>?retryWrites=true&w=majority
 mongoose
-  .connect("mongodb+srv://rahul_baghel:Rahul$125@cluster0.mrbxl.mongodb.net/logindemo?retryWrites=true&w=majority", {
+  .connect("mongodb://localhost:27017/logindemo", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
